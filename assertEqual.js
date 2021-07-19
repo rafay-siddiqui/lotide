d0f9d38138b32a06282aed1356d18514ca94fb0c
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-19 22:44:36 +0000

    Added a colouful pass/fail function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅😁✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑😔🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//TEST CODE
+assertEqual("LighthouseLabs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("identical string", "identical string");
+assertEqual("non-identical string", "identical string");
+assertEqual(1, 11);
