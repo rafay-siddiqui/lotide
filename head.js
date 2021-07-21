10cd4e254466240242348021505c9ce382793627
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-21 01:30:14 +0000

    Was not committed

--- head.js
+++ head.js
@@ -10,7 +10,7 @@ const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["only element"]),"only element");
-assertEqual(head([]),undefined);
+assertEqual(head(["only element"]), "only element");
+assertEqual(head([]), undefined);
