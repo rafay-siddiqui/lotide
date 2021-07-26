21d0b9d0735136c61aad3aa7ca0c3e721cac9d55
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-26 19:00:24 +0000

    Refactored code to make head and assertEqual more DRY

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//TEST CODE
-assertEqual("LighthouseLabs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("identical string", "identical string");
-assertEqual("non-identical string", "identical string");
-assertEqual(1, 11);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+//TEST CODE
+assertEqual("LighthouseLabs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("identical string", "identical string");
+assertEqual("non-identical string", "identical string");
+assertEqual(1, 11);
