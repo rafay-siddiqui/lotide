824d26c0fcd4474d21652bfee91de3bfa020bdf3
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-07-26 22:45:08 +0000

    Integrated all lotide functions into one index.js file

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const letterPositions = require('./letterPositions');
+const map = require('./map')
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
   head: head,
   tail: tail,
-  middle: middle
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  letterPositions: letterPositions,
+  map: map;
+  takeUntil: takeUntil,
+  without: without
 };
